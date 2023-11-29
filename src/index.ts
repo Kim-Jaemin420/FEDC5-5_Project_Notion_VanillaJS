c1cca107750a11c89f390373e73a52ec9725a740
Kim-Jaemin420 <jany240@naver.com>
2023-11-29 17:36:13 +0900

    Modify: 기존 렌더 함수 제거, 전역 이벤트 바인딩 제거

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,14 @@ import "@/styles/index.scss";
 import { createComponent } from "./core";
 import App from "./App";
 
-export function render() {
-  const $app = document.getElementById("app");
+const $app = document.getElementById("app");
 
-  if (!$app) {
-    document.body.innerHTML = "애플리케이션을 불러오는 데 문제가 발생했습니다.";
+if (!$app) {
+  document.body.innerHTML = "애플리케이션을 불러오는 데 문제가 발생했습니다.";
 
-    throw new Error("애플리케이션을 불러오는 데 문제가 발생했습니다.");
-  }
-
-  const componentInstance = createComponent(App);
-  $app.innerHTML = componentInstance.element;
-  componentInstance.bindEvents?.();
+  throw new Error("애플리케이션을 불러오는 데 문제가 발생했습니다.");
 }
 
-window.addEventListener("popstate", () => {
-  render();
-});
-
-window.addEventListener("navigate", () => {
-  render();
-});
-
-render();
+const componentInstance = createComponent(App);
+$app.innerHTML = componentInstance.element;
+componentInstance.bindEvents?.();
